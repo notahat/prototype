fd07223f7878c30a38e189001e6b0c2a89d1505c
jdalton <john.david.dalton@gmail.com>
2008-05-28 10:12:32 -0500

    Ensure deprecated Position.* methods use Element#getDimensions.

--- src/deprecated.js
+++ src/deprecated.js
@@ -51,38 +51,42 @@ var Position = {
   within: function(element, x, y) {
     if (this.includeScrollOffsets)
       return this.withinIncludingScrolloffsets(element, x, y);
+      
+    var dimensions = Element.getDimensions(element);
     this.xcomp = x;
     this.ycomp = y;
     this.offset = Element.cumulativeOffset(element);
 
     return (y >= this.offset[1] &&
-            y <  this.offset[1] + element.offsetHeight &&
+            y <  this.offset[1] + dimensions.height &&
             x >= this.offset[0] && 
-            x <  this.offset[0] + element.offsetWidth);
+            x <  this.offset[0] + dimensions.width);
   },
 
   withinIncludingScrolloffsets: function(element, x, y) {
-    var offsetcache = Element.cumulativeScrollOffset(element);
+    var offsetcache = Element.cumulativeScrollOffset(element),
+    dimensions = Element.getDimensions(element);
 
     this.xcomp = x + offsetcache[0] - this.deltaX;
     this.ycomp = y + offsetcache[1] - this.deltaY;
     this.offset = Element.cumulativeOffset(element);
 
     return (this.ycomp >= this.offset[1] &&
-            this.ycomp <  this.offset[1] + element.offsetHeight &&
+            this.ycomp <  this.offset[1] + dimensions.height &&
             this.xcomp >= this.offset[0] && 
-            this.xcomp <  this.offset[0] + element.offsetWidth);
+            this.xcomp <  this.offset[0] + dimensions.width);
   },
 
   // within must be called directly before
-  overlap: function(mode, element) {  
+  overlap: function(mode, element) {
+    var dimensions = Element.getDimensions(element);
     if (!mode) return 0;  
     if (mode == 'vertical') 
-      return ((this.offset[1] + element.offsetHeight) - this.ycomp) / 
-        element.offsetHeight;
+      return ((this.offset[1] + dimensions.height) - this.ycomp) / 
+        dimensions.height;
     if (mode == 'horizontal')
-      return ((this.offset[0] + element.offsetWidth) - this.xcomp) / 
-        element.offsetWidth;
+      return ((this.offset[0] + dimensions.width) - this.xcomp) / 
+        dimensions.width;
   },
 
   // Deprecation layer -- use newer Element methods now (1.5.2).
