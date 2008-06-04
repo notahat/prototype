ae16ee8f2a4d84e8612a531be29676532128c82c
Andrew Dupont <prototype@andrewdupont.net>
2008-06-04 14:03:10 -0500

    Remove duplicate method declarations in dom.js.

--- src/dom.js
+++ src/dom.js
@@ -516,33 +516,6 @@ Element.Methods = {
     return element;
   },
 
-  cumulativeOffset: function(element) {
-    element = $(element);
-    var valueT = 0, valueL = 0;
-    do {
-      valueT += element.offsetTop  || 0;
-      valueL += element.offsetLeft || 0;
-      element = element.offsetParent;
-    } while (element);
-    return Element._returnOffset(valueL, valueT);
-  },
-
-  positionedOffset: function(element) {
-    element = $(element);
-    var valueT = 0, valueL = 0;
-    do {
-      valueT += element.offsetTop  || 0;
-      valueL += element.offsetLeft || 0;
-      element = element.offsetParent;
-      if (element) {
-        if (element.tagName.toUpperCase() == 'BODY') break;
-        var p = Element.getStyle(element, 'position');
-        if (p !== 'static') break;
-      }
-    } while (element);
-    return Element._returnOffset(valueL, valueT);
-  },
-
   absolutize: function(element) {
     element = $(element);
     if (element.getStyle('position') == 'absolute') return element;
